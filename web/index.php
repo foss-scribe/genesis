807be2530df0f7184171d283406c6d9e138e6c06
Chris <chris@gardiner-bill.com>
2015-06-11 14:53:25 +1000

    updated .gitignore to suit silex and app structure added twi to composer.json added new genesis.sh to start the web server added webui frontend added basic app index.php

---
+++ web/index.php
@@ -0,0 +1,31 @@
+<?php
+
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+//declare app
+$app = new Silex\Application();
+
+//enable debug mode
+$app['debug'] = true;
+
+
+//load app/providers.php
+require_once __DIR__.'/../app/src/providers.php';
+
+
+//load config		
+$loadConfig = json_decode(file_get_contents(__DIR__.'/../app/config/settings.json'), true);
+
+//load routes
+require_once __DIR__.'/../app/src/routes.php';
+
+
+$filename = __DIR__.preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
+if (php_sapi_name() === 'cli-server' && is_file($filename)) {
+    return false;
+}
+
+$app->run();
+
+?>
