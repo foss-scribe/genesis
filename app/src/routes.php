f202b33c5e7587f663246625b03e7490067905ef
Chris <chris@gardiner-bill.com>
2015-06-13 23:23:55 +1000

    added the beginnings of the create world controller and view

--- app/src/routes.php
+++ app/src/routes.php
@@ -5,16 +5,25 @@ $app->get('/', function() use ($app)
 
 $config = $app['session']->get('config');
 
+//set templates
 $templates = $app['session']->get('templates');
+
+$worldTemplates = json_decode(file_get_contents($templates[1]), true);
 
 $worlds = $app['session']->get('worlds');
 
 if (empty($worlds))
 {
+	
+	//echo "<pre>";
+	//print_r($worldTemplates);
+	//echo "</pre>";
+
 	return $app['twig']->render('form_create_world.twig', array(
 			'page_title' => 'Genesis',
 			'title' => 'Genesis',
-			'config' => $config
+			'config' => $config,
+			'fieldset' => $worldTemplates['default']
 		));
 } elseif (!$config['current_world'])
 {
